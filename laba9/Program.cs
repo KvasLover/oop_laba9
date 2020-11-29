af09cca3e772bbcbd340538b2399babb1f4374a7
KvasLover <vadimkurb22@gmail.com>
2020-11-29 13:57:14 +0300

    Добавьте файлы проекта.

---
+++ laba9/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace laba9
+{
+    class Account
+    {
+        public delegate void AccountHandler(string message);
+        public event AccountHandler Notify;              // 1.Определение события
+        public Account(int sum)
+        {
+            Sum = sum;
+        }
+        public int Sum { get; private set; }
+        public void Put(int sum)
+        {
+            Sum += sum;
+            Notify?.Invoke($"На счет поступило: {sum}");   // 2.Вызов события 
+        }
+        public void Take(int sum)
+        {
+            if (Sum >= sum)
+            {
+                Sum -= sum;
+                Notify?.Invoke($"Со счета снято: {sum}");   // 2.Вызов события
+            }
+            else
+            {
+                Notify?.Invoke($"Недостаточно денег на счете. Текущий баланс: {Sum}"); ;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(100);
+            acc.Notify += DisplayMessage;   // Добавляем обработчик для события Notify
+            acc.Put(20);    // добавляем на счет 20
+            Console.WriteLine($"Сумма на счете: {acc.Sum}");
+            acc.Take(70);   // пытаемся снять со счета 70
+            Console.WriteLine($"Сумма на счете: {acc.Sum}");
+            acc.Take(180);  // пытаемся снять со счета 180
+            Console.WriteLine($"Сумма на счете: {acc.Sum}");
+        }
+        private static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
